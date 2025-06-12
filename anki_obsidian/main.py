280e40f334d0dd84c318dbfa5a7f23e03d32d11b
Enrico Bolzonello <enribolzonello@gmail.com>
2025-06-12 17:19:25 +0200

    feat: interaction with anki complete

--- anki_obsidian/main.py
+++ anki_obsidian/main.py
@@ -4,6 +4,8 @@ import urllib.request
 import argparse
 from parsing import bfs
 
+TEMP_FILE = os.path.dirname(__file__) + '/cards.txt'
+
 def request(action, **params):
     return {"action": action, "params": params, "version": 6}
 
@@ -25,12 +27,6 @@ def invoke(action, **params):
         raise Exception(response["error"])
     return response["result"]
 
-
-# invoke('createDeck', deck='test1')
-# result = invoke('deckNames')
-# print('got list of decks: {}'.format(result))
-
-
 def main():
     parser = argparse.ArgumentParser(
         prog="Obsidian->Anki flashcards",
@@ -48,7 +44,51 @@ def main():
         for q, a in cards:
             print(f"Q: {q.strip()} \nA: {a.strip()}\n")
 
+    deck_names = invoke('deckNames')
+    existing_notes = set()
+    if args.filename not in deck_names:
+        invoke('createDeck', deck=args.filename)
+        print(f"✅  Creating new deck {args.filename}.")
+    else:
+        result = invoke('notesInfo', query=f"deck:{args.filename}")
+        for existing_note in result:
+            question = existing_note["fields"]["Front"]["value"]
+            answer = existing_note["fields"]["Back"]["value"]
 
+            for q, a in cards:
+                note_id = existing_note["noteId"]
+                
+                if q==question and a==answer:
+                    existing_notes.add((q, a))
+                elif q == question or a == answer:
+                    new_front = question if q != question else q
+                    new_back = answer if a != answer else a
+
+                    print(f"⚠️  Updating note with new front {new_front} and back {new_back}.")
+                    
+                    invoke("updateNoteFields", note={
+                        "id": note_id,
+                        "fields": {
+                            "Front": new_front,
+                            "Back": new_back
+                        }
+                    })
+                    existing_notes.add((q, a))
+
+
+    with open(TEMP_FILE, 'w') as f:   
+        f.write(f"#deck:{args.filename}\n")
+        count = 0
+        for q,a in cards:
+            if (q,a) in existing_notes:
+                continue
+            f.write(f"{q};{a}\n")
+            count += 1
+    if count > 0:
+        invoke('guiImportFile', path=TEMP_FILE)
+    else:
+        print("⚠️  Skipping empty file.")
+    os.remove(TEMP_FILE)
 
 if __name__ == "__main__":
     main()
